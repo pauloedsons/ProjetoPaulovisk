512329e538c97f1186fdd9f8119d602b8e7bf4ff
Paulo Edson <paulo_edsons@hotmail.com>
2016-07-13 10:06:43 -0300

    double valorInvestido = 1000.0;

---
+++ Caelum1/Caelum1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Caelum1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double valorInvestido = 1000.0;
+            int i = 1;
+            while (i <= 12) {
+                valorInvestido = valorInvestido * 1.01;
+                i += 1;
+                Console.WriteLine("Valor investido agora é " + valorInvestido);
+            }
+            
+
+            Console.ReadKey();
+
+        }
+    }
+}
