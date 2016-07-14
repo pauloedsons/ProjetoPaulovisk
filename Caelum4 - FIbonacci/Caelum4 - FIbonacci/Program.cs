1228c08128f39e3494d24ee823eb96c996d9b371
Paulo Edson <paulo_edsons@hotmail.com>
2016-07-14 04:20:42 -0300

    Fibonacci

---
+++ Caelum4 - FIbonacci/Caelum4 - FIbonacci/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Caelum4___FIbonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int fibonacci = 0;
+
+            for(int i = 1; i < 100; i++)
+            {
+                i +=1;
+
+                fibonacci *= i ;
+               
+
+                Console.WriteLine("Fibonacci:"+fibonacci);
+          }
+            Console.ReadKey();
+        }
+    }
+}
