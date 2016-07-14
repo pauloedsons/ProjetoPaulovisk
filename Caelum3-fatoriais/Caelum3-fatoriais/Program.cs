bd554df5c02db78e14e4c6f1c40cd874ae517141
Paulo Edson <paulo_edsons@hotmail.com>
2016-07-14 03:49:10 -0300

    Numeros fatoriais de 1 a 10

---
+++ Caelum3-fatoriais/Caelum3-fatoriais/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Caelum3_fatoriais
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int fatorial = 1;
+            
+            for (int n = 1; n <= 10; n++)
+            {
+                
+                    fatorial += n -1;
+                
+
+
+                Console.WriteLine("Fatorial: " + fatorial );
+            }
+            Console.ReadKey();
+            
+
+           
+            
+
+         
+        }
+    }
+}
