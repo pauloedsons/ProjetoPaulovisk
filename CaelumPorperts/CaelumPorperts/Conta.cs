2b4f7f3084da1068b0055d7854bcbf5188f77a06
Paulo Edson <paulo_edsons@hotmail.com>
2016-07-31 04:00:08 -0300

    // Transforme os atributos da classe Conta em propriedades.     //Permita que o saldo da conta seja lido,porém     //não seja alterado fora da classe, altere também o código das classes     //que utilizam a conta para que elas acessem     //as propriedades ao invés dos atributos diretamente.

---
+++ CaelumPorperts/CaelumPorperts/Conta.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaelumPorperts
+{
+
+    // Transforme os atributos da classe Conta em propriedades. 
+    //Permita que o saldo da conta seja lido,porém 
+    //não seja alterado fora da classe, altere também o código das classes 
+    //que utilizam a conta para que elas acessem 
+    //as propriedades ao invés dos atributos diretamente.
+
+
+    class Conta
+    {
+        private int numero = 20;
+        private int saldo = 53;
+                      
+       // propriedade 'Numero'
+
+        public int Numero
+        {
+            get
+            {
+
+                return this.numero;
+
+            }
+            set
+            {
+
+            }
+             
+        }
+        //propriedade 'Saldo'
+
+        public int Saldo
+        {
+            get
+            {
+
+                return this.saldo;
+
+            }
+            private set
+            {
+
+            } 
+
+        } 
+ 
+        
+
+        //metodo para ler a propriedade
+
+        public void MetodoParaLeitura()
+        {
+            Conta c = new Conta(); 
+
+            Console.WriteLine("resultado:" +  c.Numero);
+
+            Console.WriteLine("resultado:" + c.Saldo);
+
+
+        }
+
+
+    }
+}
---
+++ CaelumPorperts/CaelumPorperts/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaelumPorperts
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Conta c = new Conta();
+
+
+           
+            c.MetodoParaLeitura();
+            Console.ReadKey();
+        }
+    }
+}
