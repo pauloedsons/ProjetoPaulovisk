218ccb2adad9dfdbd0d2f3d6f9bc36a627c723b4
Paulo Edson <paulo_edsons@hotmail.com>
2016-07-31 07:03:10 -0300

    ...

---
+++ Construtores/Construtores/Cliente.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Construtores
+{
+    class Cliente
+    {
+
+        private int idade;
+        private int numero;
+        private string nome;
+
+     public Cliente (int idade, int numero, string nome)
+        {
+
+
+
+        }
+
+
+    }
+}
